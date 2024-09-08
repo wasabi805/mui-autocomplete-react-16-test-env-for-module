69283fccdbeec86da5dcb071c98825331ab6709e
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2024-09-07 17:42:03 -0700

    save notes

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'main.js',
+    filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
 
